8bbc34b50e5eda1551386a350f41af106fe331a4
Rihan Naufaldihanif <rihannaufal22@gmail.com>
2024-12-20 14:22:26 +0700

    feat: add edit permintaan form

---
+++ src/components/PermintaanEditForm.tsx
@@ -0,0 +1,172 @@
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {Button} from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {Popover, PopoverContent, PopoverTrigger} from './ui/popover';
+import {CalendarIcon} from 'lucide-react';
+import {Calendar} from './ui/calendar';
+import {format} from 'date-fns';
+
+const permintaanSchema = z.object({
+  tanggal: z.date({
+    required_error: 'Jadwal tidak boleh kosong.',
+  }),
+  kode: z.string().min(1, {message: 'Kode Barang is required'}),
+  nama: z.string().min(1, {message: 'Nama Barang is required'}),
+  satuan: z.string().min(1, {message: 'Satuan is required'}),
+  jumlah: z.number().min(1, {message: 'Jumlah must be at least 1'}),
+  ruangan: z.string().min(1, {message: 'Ruangan is required'}),
+  status: z.enum(['pending', 'approved', 'rejected'], {
+    errorMap: () => ({
+      message: "Status must be 'pending', 'approved', or 'rejected'",
+    }),
+  }),
+});
+
+export default function PermintaanEdit() {
+  const form = useForm<z.infer<typeof permintaanSchema>>({
+    resolver: zodResolver(permintaanSchema),
+    defaultValues: {
+      tanggal: new Date(),
+      kode: '',
+      nama: '',
+      satuan: '',
+      jumlah: 0,
+      ruangan: '',
+      status: 'pending',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof permintaanSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className=' space-y-8'>
+        <FormField
+          control={form.control}
+          name='tanggal'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Tanggal Permintaan</FormLabel>
+              <FormControl>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={'outline'}
+                      className='flex justify-start font-normal'
+                    >
+                      <CalendarIcon />
+                      {field.value ? (
+                        format(field.value, 'PPP')
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className='w-auto p-0'>
+                    <Calendar
+                      mode='single'
+                      selected={field.value}
+                      onSelect={(date) => field.onChange(date)}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='kode'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Kode Barang</FormLabel>
+              <FormControl>
+                <Input placeholder='Kode Barang' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='nama'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Nama Barang</FormLabel>
+              <FormControl>
+                <Input placeholder='Nama Barang' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='satuan'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Satuan</FormLabel>
+              <FormControl>
+                <Input placeholder='Satuan' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='jumlah'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Jumlah</FormLabel>
+              <FormControl>
+                <Input placeholder='Jumlah' type='number' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='ruangan'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Ruangan</FormLabel>
+              <FormControl>
+                <Input placeholder='Ruangan' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='status'
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Status</FormLabel>
+              <FormControl>
+                <Input placeholder='Status' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- src/lib/columns/barang-permintaan-column.tsx
+++ src/lib/columns/barang-permintaan-column.tsx
@@ -1,8 +1,9 @@
 import {ColumnDef} from '@tanstack/react-table';
 import {Pencil} from 'lucide-react';
 import {BarangTransaction} from '@/lib/types/barang';
-import {Link} from 'react-router-dom';
 import {Badge} from '@/components/ui/badge';
+import {Dialog, DialogContent, DialogHeader, DialogTrigger} from '@/components/ui/dialog';
+import PermintaanEdit from '@/components/PermintaanEditForm';
 
 export const barangPermintaanColumns: ColumnDef<BarangTransaction>[] = [
   {
@@ -48,26 +49,31 @@ export const barangPermintaanColumns: ColumnDef<BarangTransaction>[] = [
   {
     header: 'Action',
     cell: () => (
-      <div className='flex space-x-3 justify-center items-center'>
-        {/* <Link to={'/'} className='bg-green-500 p-1 rounded-md hover:bg-green-900/90'>
+      <Dialog>
+        <div className='flex space-x-3 justify-center items-center'>
+          {/* <Link to={'/'} className='bg-green-500 p-1 rounded-md hover:bg-green-900/90'>
           <CirclePlus size={18} color='white' />
         </Link> */}
-        <Link
-          to={'/'}
-          className='bg-yellow-500 p-1 rounded-md hover:bg-yellow-900/90'
-        >
-          <Pencil size={18} color='white' />
-        </Link>
-        {/* <Link to={'/'} className='bg-sky-500 p-1 rounded-md hover:bg-sky-900/90'>
+          <DialogTrigger className='bg-yellow-500  h-fit p-1 rounded-md hover:bg-yellow-900/90'>
+            <Pencil size={18} color='white' />
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader className='text-xl font-semibold'>
+              Edit Permintaan
+            </DialogHeader>
+            <PermintaanEdit />
+          </DialogContent>
+          {/* <Link to={'/'} className='bg-sky-500 p-1 rounded-md hover:bg-sky-900/90'>
           <Download size={18} color='white' />
         </Link> */}
-        {/* <Link
+          {/* <Link
           to={'/'}
           className='bg-red-500 p-1 rounded-md hover:bg-red-900/90'
         >
           <Trash2 size={18} color='white' />
         </Link> */}
-      </div>
+        </div>
+      </Dialog>
     ),
   },
 ];
